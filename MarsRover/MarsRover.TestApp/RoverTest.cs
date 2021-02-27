2c186aededdaa54f6e12cedf83e0df764596db72
Sinan Tok <sinantok@outlook.com>
2021-02-27 20:27:17 +0300

    rover test operations

---
+++ MarsRover/MarsRover.TestApp/RoverTest.cs
@@ -0,0 +1,28 @@
+﻿using MarsRover.ConsoleApp.Helpers;
+using MarsRover.ConsoleApp.Models;
+using MarsRover.ConsoleApp.Models.Enums;
+using NUnit.Framework;
+
+namespace MarsRover.TestApp
+{
+    public class RoverTest
+    {
+        RoverHelper _roverHelper;
+        Plateau _plateau;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("LMLMLMLMM", "1 3 N")]
+        public void Test(string movement, string position)
+        {
+            _plateau = new Plateau(new Coordinate(5, 5));
+            _roverHelper = new RoverHelper(Direction.N, _plateau, new Coordinate(1, 2));
+            _roverHelper.Operations(movement);
+            string result = string.Format("{0} {1} {2}", _roverHelper._ICoordinate.XCoordinate, _roverHelper._ICoordinate.YCoordinate, _roverHelper.Direction);
+
+            Assert.AreEqual(result, position);
+        }
+    }
+}
